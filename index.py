69cc536ff38017e4f4660ea11a4b5a47f63f8f6c
Eduardo Arruda <eduardoarruda2001@outlook.com>
2024-01-18 20:09:07 -0300

    Impar ou Par

---
+++ index.py
@@ -0,0 +1,10 @@
+print("Par ou Impar?\n")
+numero = int(input("Qual numero você quer checar?\n"))
+
+if numero % 2 == 0:
+    print("\nPar\n")
+else:
+    print("\nImpar\n")
+
+
+enter = input("Aperte Enter para fechar")
